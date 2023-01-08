d7125e03f90f2d16b343c150e710d14d9cab93d5
Andrew Hamon <and.ham95@gmail.com>
2023-01-07 17:00:05 -0800

    Use flakes

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Packages and modules for Authfish";
+  
+  inputs.nixpkgs.url = "nixpkgs/nixos-22.11";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        packages = rec {
+          authfish = import ./default.nix { inherit pkgs; };
+          default = authfish;
+        };
+
+        devShells.default = import ./shell.nix { inherit pkgs; };
+      }
+    ) // {
+      nixosModules.default = { pkgs, ... }: {
+        nixpkgs.overlays = [
+          (self: super:
+          {
+            authfish = import ./default.nix { inherit pkgs; };
+          })
+        ];
+        imports = [
+          ./nix/modules/authfish.nix
+        ];
+      };
+    };
+}
---
+++ nix/modules/authfish.nix
@@ -0,0 +1,98 @@
+{ lib, pkgs, config, ... }:
+with lib;
+let
+  cfg = config.services.authfish;
+in
+{
+  options = {
+    services.authfish = {
+      enable = mkEnableOption "Authfish";
+
+      dataDir = mkOption {
+        type = types.str;
+        default = "/var/lib/authfish/";
+        description = "The directory where Authfish stores its data files.";
+      };
+
+      port = mkOption {
+        type = types.int;
+        default = 8478;
+      };
+
+      user = mkOption {
+        type = types.str;
+        default = "authfish";
+      };
+
+      group = mkOption {
+        type = types.str;
+        default = "authfish";
+      };
+
+      domains = mkOption {
+        type = types.str;
+      };
+
+      virtualHostName = mkOption {
+        type = types.str;
+      };
+
+      enableNginx = mkOption {
+        type = types.bool;
+      };
+
+      enableACME = mkOption {
+        type = types.bool;
+      };
+
+      forceSSL = mkOption {
+        type = types.bool;
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      authfish
+    ];
+
+    systemd.services.authfish = {
+      description = "Authfish";
+      after = [ "network.target" ];
+      wantedBy = [ "multi-user.target" ];
+
+      serviceConfig = {
+        Type = "simple";
+        User = cfg.user;
+        Group = cfg.group;
+        ExecStart = "${pkgs.authfish}/bin/authfish server --port ${toString cfg.port} --domain ${cfg.domains}";
+        Restart = "on-failure";
+      };
+    };
+
+    users.users = mkIf (cfg.user == "authfish") {
+      authfish = {
+        isNormalUser = true;
+        home = cfg.dataDir;
+        group = cfg.group;
+      };
+    };
+
+    users.groups = mkIf (cfg.group == "authfish") {
+      authfish = { };
+    };
+
+    services.nginx.virtualHosts = {
+      "${cfg.virtualHostName}" = {
+        enableACME = cfg.enableACME;
+        forceSSL = cfg.forceSSL;
+        locations."/" = {
+          proxyPass = "http://localhost:${toString cfg.port}";
+          extraConfig = ''
+            proxy_set_header X-Original-URL $scheme://$http_host$request_uri;
+          '';
+        };
+      };
+    };
+  };
+}
